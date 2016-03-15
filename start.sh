2aa6cecd30b2dafb70604350affde26702dd855c
root <root@HOME>
2016-03-15 01:30:37 -0400

    t

--- start.sh
+++ start.sh
@@ -5,26 +5,6 @@
 mkdir -p -m 0700 /root/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 
-# Setup git variables
-if [ ! -z "$GIT_EMAIL" ]; then
- git config --global user.email "$GIT_EMAIL"
-fi
-if [ ! -z "$GIT_NAME" ]; then
- git config --global user.name "$GIT_NAME"
- git config --global push.default simple
-fi
-
-# Pull down code form git for our site!
-if [ ! -z "$GIT_REPO" ]; then
-  rm /usr/share/nginx/html/*
-  if [ ! -z "$GIT_BRANCH" ]; then
-    git clone -b $GIT_BRANCH $GIT_REPO /usr/share/nginx/html/
-  else
-    git clone $GIT_REPO /usr/share/nginx/html/
-  fi
-  chown -Rf nginx.nginx /usr/share/nginx/*
-fi
-
 # Display PHP error's or not
 if [[ "$ERRORS" != "1" ]] ; then
   sed -i -e "s/error_reporting =.*=/error_reporting = E_ALL/g" /etc/php5/fpm/php.ini
@@ -51,6 +31,19 @@ fi
 
 # Again set the right permissions (needed when mounting from a volume)
 chown -Rf www-data.www-data /usr/share/nginx/html/
+
+if [ ! -d /DATA/htdocs ] ; then
+  mkdir -p /DATA/htdocs
+  chown nginx:www-data /DATA/htdocs
+fi
+
+# start php-fpm
+mkdir -p /DATA/logs/php-fpm
+
+# start nginx
+mkdir -p /DATA/logs/nginx
+mkdir -p /tmp/nginx
+chown nginx /tmp/nginx
 
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
